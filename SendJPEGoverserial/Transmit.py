08534b4fe5d03ee7fff67f4c0c1860eb8b48ac6f
mrar <mrozaini.ar@gmail.com>
2016-04-03 00:19:42 +0800

    Update Transmit.py

--- SendJPEGoverserial/Transmit.py
+++ SendJPEGoverserial/Transmit.py
@@ -1,18 +1,8 @@
 import serial
-import cPickle
-import time
-
 ser = serial.Serial("COM4",9600)
 
-#ser.write(open("image2.gif","rb").read())
-#ser.write(open("image01.jpg","rb").read())
-
-#serialization
 picklestring = open("image01.jpg","rb").read()
 sizedata = str(len(picklestring))
 print sizedata
 data = sizedata+picklestring
-ts = time.time()
 ser.write(data)
-te = time.time()
-print te-ts
